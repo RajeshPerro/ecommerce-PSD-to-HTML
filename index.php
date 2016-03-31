66f81f6785e0fb215dd4db43b1b325240029f587
rajesh <rajeshghosh4007@gmail.com>
2016-03-31 11:59:27 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rajesh
+ * Date: 3/15/16
+ * Time: 4:19 PM
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>home | Page</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<!--header start-->
+<div class="header-all">
+    <div class="header-top">
+        <div class="header-top-info">
+            <ul>
+                <li><span>+8801714905039</span></li>
+                <li><span>rajesh@coderstrust.com</span></li>
+            </ul>
+        </div>
+        <div class="header-top-menu">
+            <ul>
+                <li><span>MY ACCOUNT</span></li>
+                <li><span>LOGIN</span></li>
+                <li><span>CHECKOUT</span></li>
+                <li><span>SIGN UP</span></li>
+            </ul>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="header-bottom-content">
+
+            <div class="header-bottom-logo">
+                <div class="name-div">
+                    <h1>
+                        BULLSY
+                    </h1>
+                    <p>RUGGED SHOES STORE</p>
+                </div>
+                <div class="logo-div">
+                    <img src="assets/images/logo_icon.png">
+                </div>
+            </div>
+            <div class="header-bottom-menu header-top-menu">
+                <ul>
+                    <li><span>HOME</span> <span class="slash"> / </span></li>
+                    <li><span>APPAREL</span><span class="slash"> / </span></li>
+                    <li><span>FASHION</span><span class="slash"> / </span></li>
+                    <li><span>CONTACT US</span></li>
+                </ul>
+            </div>
+            <div class="header-bottom-card">
+                <div class="header-bottom-card-text">
+                    <p>10 items</p>
+                    <h6>459.99 $</h6>
+                </div>
+                <!-- ===== -->
+                <div class="header-bottom-card-img">
+                    <img class="img-responsive" src="assets/images/cart_icon.png">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--slider part start-->
+<div class="slider-all">
+
+<!--    <img src="assets/images/ff.png">-->
+    <div class="slider-top">
+        <div class="slider-top-left">
+
+        </div>
+        <div class="slider-top-right">
+
+        </div>
+    </div>
+
+    <div class="slider-bottom">
+        <div class="slider-bottom-left">
+
+        </div>
+        <div class="slider-bottom-middle">
+
+        </div>
+        <div class="slider-bottom-right">
+
+        </div>
+    </div>
+
+</div>
+
+<!--body part start-->
+<div class="shoe-category">
+
+</div>
+<div class="shoe-latest">
+
+</div>
+<div class="shoe-featured">
+
+</div>
+<div class="shoe-latest">
+
+</div>
+
+<div class="company-add">
+
+</div>
+
+<!--Footer start-->
+<div class="footer-all">
+    <div class="footer-top">
+        <div class="footer-top-log">
+
+        </div>
+        <div class="footer-top-menu">
+
+        </div>
+        <div class="footer-top-arrow">
+
+        </div>
+
+    </div>
+    <div class="footer-bottom">
+        <div class="footer-bottom-social">
+
+        </div>
+        <div class="footer-bottom-copyright">
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
